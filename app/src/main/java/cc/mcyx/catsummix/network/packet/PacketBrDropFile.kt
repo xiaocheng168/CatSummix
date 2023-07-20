2c098ffb02b7a21445dc750384974cfc37d80f20
Zcc <x3513312668@gmail.com>
2023-07-20 10:14:51 +0800

    modify format

--- app/src/main/java/cc/mcyx/catsummix/network/packet/PacketBrDropFile.kt
+++ app/src/main/java/cc/mcyx/catsummix/network/packet/PacketBrDropFile.kt
@@ -8,14 +8,16 @@ import com.alibaba.fastjson.JSONObject
 
 //广播传递文件 用于判断需要接收这个文件
 @RequiresApi(Build.VERSION_CODES.O)
-class PacketBrDropFile(files: MutableList<ShareActivity.FileInfo>, encode: Boolean) : ImplPacketBase() {
+class PacketBrDropFile(val files: MutableList<ShareActivity.FileInfo>, encode: Boolean) : ImplPacketBase() {
+    private var jsonObject = JSONObject()
+    var fileSize: Long = 0
+    private var jsonArray = JSONArray()
+
     init {
         this.setEncode(encode)
-        val jsonObject = JSONObject()
-        var size: Long = 0
-        val jsonArray = JSONArray()
+
         for (file in files) {
-            size += file.fileSize
+            fileSize += file.fileSize
             JSONObject().apply {
                 put("name", file.fileName)
                 put("size", file.fileSize)
@@ -23,7 +25,7 @@ class PacketBrDropFile(files: MutableList<ShareActivity.FileInfo>, encode: Boole
             }
         }
         jsonObject["count"] = files.size
-        jsonObject["size"] = size
+        jsonObject["size"] = fileSize
         jsonObject["files"] = jsonArray
         this.setData(jsonObject)
         this.setType(5)
